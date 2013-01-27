39274643c5a0ccd91081492432037b50cd45e56b
Zsombor Paróczi <zsombor.paroczi@gmail.com>
2013-01-27 11:08:10 +0100

    LibNui Framework bundle

---
+++ libnui/libnui.h
@@ -0,0 +1,13 @@
+//
+//  libnui.h
+//  libnui
+//
+//  Created by Zsombor Paroczi on 1/27/13.
+//  Copyright (c) 2013 libNUI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libnui : NSObject
+
+@end
---
+++ libnui/libnui.m
@@ -0,0 +1,13 @@
+//
+//  libnui.m
+//  libnui
+//
+//  Created by Zsombor Paroczi on 1/27/13.
+//  Copyright (c) 2013 libNUI. All rights reserved.
+//
+
+#import "libnui.h"
+
+@implementation libnui
+
+@end
